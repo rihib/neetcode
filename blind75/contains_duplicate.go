9d58ed3a36a05dc6d97e36bca385d0d37e54f61b
rihib <rihib@rihib.dev>
2024-01-07 16:48:17 +0900

    fix

--- blind75/contains_duplicate.go
+++ blind75/contains_duplicate.go
@@ -2,7 +2,7 @@ func containsDuplicate(nums []int) bool {
 	if len(nums) <= 1 {
 		return false
 	}
-	nm := make(map([int]struct{}))
+	nm := make(map[int]struct{})
 	for _, v := range nums {
 		if _, ok := nm[v]; ok {
 			return true;
