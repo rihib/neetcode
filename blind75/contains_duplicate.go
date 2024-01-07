83dde473b356b45459814d62b15718391d2efd91
rihib <rihib@rihib.dev>
2024-01-07 16:47:10 +0900

    試しに１問目を解いてみた

---
+++ blind75/contains_duplicate.go
@@ -0,0 +1,13 @@
+func containsDuplicate(nums []int) bool {
+	if len(nums) <= 1 {
+		return false
+	}
+	nm := make(map([int]struct{}))
+	for _, v := range nums {
+		if _, ok := nm[v]; ok {
+			return true;
+		}
+		nm[v] = struct{}{}
+	}
+	return false
+}
