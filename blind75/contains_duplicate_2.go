d3dd604678c9236cfed0783973e8f9ad4ca4e466
rihib <rihib@rihib.dev>
2024-02-05 23:24:16 +0900

    submit another solution for contains duplicate

---
+++ blind75/contains_duplicate_2.go
@@ -0,0 +1,16 @@
+package blind75
+
+func containsDuplicate2(nums []int) bool {
+	if len(nums) <= 1 {
+		return false
+	}
+
+	m := make(map[int]struct{})
+	for _, n := range nums {
+		m[n] = struct{}{}
+	}
+	if len(nums) != len(m) {
+		return true
+	}
+	return false
+}
