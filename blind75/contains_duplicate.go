698d665652950746ebac6515b35825ebc1ed710d
rihib <rihib@rihib.dev>
2024-02-05 15:38:56 +0900

    refactor contains duplicate

--- blind75/contains_duplicate.go
+++ blind75/contains_duplicate.go
@@ -7,7 +7,7 @@ func containsDuplicate(nums []int) bool {
 
 	m := make(map[int]struct{})
 	for _, n := range nums {
-		if _, ok := m[n]; ok {
+		if _, found := m[n]; found {
 			return true
 		}
 		m[n] = struct{}{}
