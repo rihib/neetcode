af0ac440d5745d62dd222358c56a87dc03419935
rihib <rihib@rihib.dev>
2024-02-06 20:36:34 +0900

    refactor product of array except self

--- blind75/product_of_array_except_self.go
+++ blind75/product_of_array_except_self.go
@@ -3,16 +3,16 @@ package blind75
 func productExceptSelf(nums []int) []int {
 	res := make([]int, len(nums))
 
-	rp := 1
+	lp := 1
 	for i := 0; i < len(nums); i++ {
-		res[i] = rp
-		rp *= nums[i]
+		res[i] = lp
+		lp *= nums[i]
 	}
 
-	lp := 1
+	rp := 1
 	for i := len(nums) - 1; i >= 0; i-- {
-		res[i] *= lp
-		lp *= nums[i]
+		res[i] *= rp
+		rp *= nums[i]
 	}
 
 	return res
