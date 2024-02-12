0644e1c1d009c2ce03b2894d25cb3dd98ac31a48
rihib <rihib@rihib.dev>
2024-02-12 23:47:31 +0900

    submit invert binary tree

---
+++ go/invert_binary_tree.go
@@ -0,0 +1,18 @@
+package blind75
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	tmp := root.Left
+	root.Left = invertTree(root.Right)
+	root.Right = invertTree(tmp)
+	return root
+}
