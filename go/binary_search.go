53cc5df63641ee07497fe47baca948f19d18de3c
rihib <rihib@rihib.dev>
2024-02-13 00:32:51 +0900

    submit binary search

---
+++ go/binary_search.go
@@ -0,0 +1,17 @@
+package blind75
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if target < nums[mid] {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
