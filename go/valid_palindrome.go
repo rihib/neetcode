2eaae56182a7caeaa3e55ace9536306ec93394f7
rihib <rihib@rihib.dev>
2024-02-12 20:44:11 +0900

    refactor valid palindrome

--- go/valid_palindrome.go
+++ go/valid_palindrome.go
@@ -1,38 +1,29 @@
 package blind75
 
+import "unicode"
+
 func isPalindrome(s string) bool {
 	i := 0
 	j := len(s) - 1
-	for {
-		if i >= j {
-			return true
-		}
+	for i < j {
+		si := rune(s[i])
+		sj := rune(s[j])
 
-		if !(('A' <= s[i] && s[i] <= 'Z') || ('a' <= s[i] && s[i] <= 'z') || ('0' <= s[i] && s[i] <= '9')) {
+		if !(unicode.IsLetter(si) || unicode.IsDigit(si)) {
 			i++
 			continue
 		}
-		if !(('A' <= s[j] && s[j] <= 'Z') || ('a' <= s[j] && s[j] <= 'z') || ('0' <= s[j] && s[j] <= '9')) {
+		if !(unicode.IsLetter(sj) || unicode.IsDigit(sj)) {
 			j--
 			continue
 		}
 
-		if s[i] != s[j] {
-			si := s[i]
-			sj := s[j]
-			if 'A' <= s[i] && s[i] <= 'Z' {
-				si = s[i] - 'A' + 'a'
-			}
-			if 'A' <= s[j] && s[j] <= 'Z' {
-				sj = s[j] - 'A' + 'a'
-			}
-
-			if si != sj {
-				return false
-			}
+		if unicode.ToLower(si) != unicode.ToLower(sj) {
+			return false
 		}
 
 		i++
 		j--
 	}
+	return true
 }
