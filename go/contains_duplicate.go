a1df1370890ad2377f3a6d52186a55649056142c
rihib <rihib@rihib.dev>
2024-02-15 21:10:57 +0900

    refactor contains duplicate

--- go/contains_duplicate.go
+++ go/contains_duplicate.go
@@ -1,10 +1,6 @@
 package blind75
 
 func containsDuplicate(nums []int) bool {
-	if len(nums) <= 1 {
-		return false
-	}
-
 	m := make(map[int]struct{})
 	for _, n := range nums {
 		if _, ok := m[n]; ok {
