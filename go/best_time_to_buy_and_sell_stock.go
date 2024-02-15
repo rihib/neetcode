ae39e6aa28dc1debc58ac62b3586c0f0c88fa22b
rihib <rihib@rihib.dev>
2024-02-15 19:22:05 +0900

    refactor best time to buy and sell stock

--- go/best_time_to_buy_and_sell_stock.go
+++ go/best_time_to_buy_and_sell_stock.go
@@ -4,12 +4,10 @@ import "math"
 
 func maxProfit(prices []int) int {
 	res := 0
-	minPrice := math.MaxUint32
-
+	minProfit := math.MaxUint32
 	for _, price := range prices {
-		minPrice = min(price, minPrice)
-		res = max(price-minPrice, res)
+		res = max(res, price-minProfit)
+		minProfit = min(minProfit, price)
 	}
-
 	return res
 }
