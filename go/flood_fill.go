871185f2f82b5d106b8000168c164b0945288c60
rihib <rihib@rihib.dev>
2024-02-19 01:02:16 +0900

    submit flood fill

--- go/flood_fill.go
+++ go/flood_fill.go
@@ -3,11 +3,15 @@ package main
 
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	originalColor := image[sr][sc]
+	if originalColor == color {
+		return image
+	}
+
 	image[sr][sc] = color
 	if 0 <= sr-1 && image[sr-1][sc] == originalColor {
 		image = floodFill(image, sr-1, sc, color)
 	}
-	if sr+1 < len(image[sr]) && image[sr+1][sc] == originalColor {
+	if sr+1 < len(image) && image[sr+1][sc] == originalColor {
 		image = floodFill(image, sr+1, sc, color)
 	}
 	if 0 <= sc-1 && image[sr][sc-1] == originalColor {
--- go/main.go
+++ go/main.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	image := floodFill([][]int{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2)
-	for _, v := range image {
-		for _, b := range v {
-			fmt.Println(b)
-		}
-	}
+	fmt.Println("Hello, World!")
 }
