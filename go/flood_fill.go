e64fa956dd7b4feefdf765283d74e603247cd040
rihib <rihib@rihib.dev>
2024-02-19 02:06:35 +0900

    refactor flood fill

--- go/flood_fill.go
+++ go/flood_fill.go
@@ -3,22 +3,21 @@ package main
 
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	originalColor := image[sr][sc]
+
 	if originalColor == color {
 		return image
 	}
-
 	image[sr][sc] = color
-	if 0 <= sr-1 && image[sr-1][sc] == originalColor {
-		image = floodFill(image, sr-1, sc, color)
+
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+	for _, d := range directions {
+		nSr, nSc := sr+d[0], sc+d[1]
+		if 0 <= nSr && nSr < len(image) &&
+			0 <= nSc && nSc < len(image[0]) &&
+			image[nSr][nSc] == originalColor {
+			image = floodFill(image, nSr, nSc, color)
+		}
 	}
-	if sr+1 < len(image) && image[sr+1][sc] == originalColor {
-		image = floodFill(image, sr+1, sc, color)
-	}
-	if 0 <= sc-1 && image[sr][sc-1] == originalColor {
-		image = floodFill(image, sr, sc-1, color)
-	}
-	if sc+1 < len(image[sr]) && image[sr][sc+1] == originalColor {
-		image = floodFill(image, sr, sc+1, color)
-	}
+
 	return image
 }
