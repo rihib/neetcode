93e85b095013d867918b1b6e74b4bbb53dcaaa60
rihib <rihib@rihib.dev>
2024-04-14 23:09:08 +0900

    Lowest Common Ancestor of a Binary Search Tree

--- go/invert_binary_tree.go
+++ go/invert_binary_tree.go
@@ -1,12 +1,6 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func invertTree(root *TreeNode) *TreeNode {
 	if root != nil {
 		tmp := root.Left
