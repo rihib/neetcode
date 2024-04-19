28441eb439e221494d43253cc5612b092b1c6973
rihib <rihib@rihib.dev>
2024-04-19 11:28:42 +0900

    refactor(first bad version)

--- go/first_bad_version.go
+++ go/first_bad_version.go
@@ -8,16 +8,14 @@ func isBadVersion(version int) bool {
 }
 
 func firstBadVersion(n int) int {
-	fbv := n
-	h, t := 1, n
-	for h <= t {
-		mid := (h + t) / 2
+	l, r := 1, n
+	for l < r {
+		mid := (l + r) / 2
 		if isBadVersion(mid) {
-			fbv = mid
-			t = mid - 1
+			r = mid
 		} else {
-			h = mid + 1
+			l = mid + 1
 		}
 	}
-	return fbv
+	return l
 }
