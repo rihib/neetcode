9b50cfcc8599dad556b68b1f4ffbf967ca0d61cf
rihib <rihib@rihib.dev>
2024-06-05 00:37:33 +0900

    refactor roman to integer

--- go/roman_to_integer.go
+++ go/roman_to_integer.go
@@ -3,34 +3,25 @@ package main
 
 func romanToInt(s string) int {
 	symbols := map[string]int{
-		"I":  1,
-		"IV": 4,
-		"V":  5,
-		"IX": 9,
-		"X":  10,
-		"XL": 40,
-		"L":  50,
-		"XC": 90,
-		"C":  100,
-		"CD": 400,
-		"D":  500,
-		"CM": 900,
-		"M":  1000,
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
 	}
 
-	result := 0
+	res := 0
 	runes := []rune(s)
-	for i := 0; i < len(s); i++ {
-		if i+1 < len(runes) && symbols[string(runes[i:i+2])] != 0 {
-			result += symbols[string(runes[i:i+2])]
-			i++
-			continue
-		}
-
-		if symbols[string(runes[i])] != 0 {
-			result += symbols[string(runes[i])]
+	for i := 0; i < len(runes); i++ {
+		if i+1 < len(runes) &&
+			symbols[string(runes[i])] < symbols[string(runes[i+1])] {
+			res -= symbols[string(runes[i])]
+		} else {
+			res += symbols[string(runes[i])]
 		}
 	}
 
-	return result
+	return res
 }
