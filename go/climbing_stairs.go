56929b231eaa12956506279145020ac22e729b77
rihib <rihib@rihib.dev>
2024-06-06 13:01:42 +0900

    refactor Climbing Stairs

--- go/climbing_stairs.go
+++ go/climbing_stairs.go
@@ -2,8 +2,8 @@
 package main
 
 func climbStairs(n int) int {
-	prev, curr := 0, 1
-	for i := 0; i < n; i++ {
+	prev, curr := 1, 1
+	for i := 1; i < n; i++ {
 		prev, curr = curr, prev+curr
 	}
 	return curr
