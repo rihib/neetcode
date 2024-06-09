5723c72f9ab000276c1a2b55f293a2a720fcfa01
rihib <rihib@rihib.dev>
2024-06-09 16:30:52 +0900

    refactor Same Tree

--- go/same_tree.go
+++ go/same_tree.go
@@ -6,17 +6,10 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
 	}
-	if p == nil && q != nil || p != nil && q == nil {
+
+	if p == nil || q == nil || p.Val != q.Val {
 		return false
 	}
 
-	if p.Val != q.Val {
-		return false
-	}
-
-	if !(isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)) {
-		return false
-	}
-
-	return true
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
