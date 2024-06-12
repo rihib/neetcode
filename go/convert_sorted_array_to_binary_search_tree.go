baaf81b004a0f891ff08e1b1faeec3042ca8a930
rihib <rihib@rihib.dev>
2024-06-13 01:41:52 +0900

    refactor Convert Sorted Array to Binary Search Tree

--- go/convert_sorted_array_to_binary_search_tree.go
+++ go/convert_sorted_array_to_binary_search_tree.go
@@ -2,18 +2,14 @@
 package main
 
 func sortedArrayToBST(nums []int) *TreeNode {
-	return buildBST(nums, 0, len(nums)-1)
-}
-
-func buildBST(nums []int, left int, right int) *TreeNode {
-	if left > right {
+	if len(nums) == 0 {
 		return nil
 	}
 
-	mid := left + (right-left)/2
-	node := &TreeNode{Val: nums[mid]}
-	node.Left = buildBST(nums, left, mid-1)
-	node.Right = buildBST(nums, mid+1, right)
-
-	return node
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
 }
