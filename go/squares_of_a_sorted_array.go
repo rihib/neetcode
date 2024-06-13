a5073d2c43a62a8aaf08a8762ad04575dc9107ef
rihib <rihib@rihib.dev>
2024-06-13 18:00:00 +0900

    refactor Squares of a Sorted Array

--- go/squares_of_a_sorted_array.go
+++ go/squares_of_a_sorted_array.go
@@ -1,35 +1,20 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
+import "math"
+
 func sortedSquares(nums []int) []int {
-	// [-4,-1,0,3,10]
-	startIdx := 0
-	for ; startIdx < len(nums); startIdx++ {
-		if nums[startIdx] >= 0 {
-			break
-		}
-	}
-
-	var sorted []int
-	for l, r := startIdx-1, startIdx; 0 <= l || r < len(nums); {
-		if l < 0 {
-			sorted = append(sorted, nums[r]*nums[r])
-			r++
-			continue
-		}
-		if len(nums) <= r {
-			sorted = append(sorted, nums[l]*nums[l])
-			l--
-			continue
-		}
-
-		if nums[l]*nums[l] <= nums[r]*nums[r] {
-			sorted = append(sorted, nums[l]*nums[l])
-			l--
+	sq := make([]int, len(nums))
+	i := len(nums) - 1
+	for l, r := 0, len(nums)-1; l <= r; {
+		if math.Abs(float64(nums[l])) > math.Abs(float64(nums[r])) {
+			sq[i] = nums[l] * nums[l]
+			l++
 		} else {
-			sorted = append(sorted, nums[r]*nums[r])
-			r++
+			sq[i] = nums[r] * nums[r]
+			r--
 		}
+		i--
 	}
-	return sorted
+	return sq
 }
