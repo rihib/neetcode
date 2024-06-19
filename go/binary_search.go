2084b03c8b3e8f822f7dce752069a490fdef2a96
rihib <rihib@rihib.dev>
2024-06-19 16:57:38 +0900

    refactor Binary Search

--- go/binary_search.go
+++ go/binary_search.go
@@ -2,16 +2,20 @@
 package main
 
 func search(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for left <= right {
-		mid := (left + right) / 2
-		if target < nums[mid] {
-			right = mid - 1
-		} else if nums[mid] < target {
-			left = mid + 1
-		} else {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+
+		if target == nums[mid] {
 			return mid
 		}
+
+		if target < nums[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
 	}
+
 	return -1
 }
