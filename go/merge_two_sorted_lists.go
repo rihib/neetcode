40e03352b6c83aa3890e251d9a4d1c6d55734e46
rihib <rihib@rihib.dev>
2024-06-22 13:22:58 +0900

    refactor Merge Two Sorted Lists

--- go/merge_two_sorted_lists.go
+++ go/merge_two_sorted_lists.go
@@ -3,22 +3,22 @@ package main
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	dummy := new(ListNode)
-	cur := dummy
+	tail := dummy
 
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
-			cur.Next, list1 = list1, list1.Next
+			tail.Next, list1 = list1, list1.Next
 		} else {
-			cur.Next, list2 = list2, list2.Next
+			tail.Next, list2 = list2, list2.Next
 		}
-		cur = cur.Next
+		tail = tail.Next
 	}
 
 	if list1 != nil {
-		cur.Next = list1
+		tail.Next = list1
 	}
 	if list2 != nil {
-		cur.Next = list2
+		tail.Next = list2
 	}
 
 	return dummy.Next
