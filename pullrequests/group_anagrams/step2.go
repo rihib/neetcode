3d7a79e4f85bf7464f394484bb1a8d7043392ae6
rihib <rihib@rihib.dev>
2024-06-25 19:23:57 +0900

    for-review: Group Anagrams

---
+++ pullrequests/group_anagrams/step2.go
@@ -0,0 +1,25 @@
+//lint:file-ignore U1000 Ignore all unused code
+package groupanagrams
+
+/*
+	変数名を改善した。
+*/
+
+func groupAnagrams_step2(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+	for _, s := range strs {
+		var freq [26]int
+		for _, r := range s {
+			freq[r-'a']++
+		}
+		m[freq] = append(m[freq], s)
+	}
+
+	res := make([][]string, len(m))
+	i := 0
+	for _, v := range m {
+		res[i] = v
+		i++
+	}
+	return res
+}
---
+++ pullrequests/group_anagrams/step3.go
@@ -0,0 +1,31 @@
+//lint:file-ignore U1000 Ignore all unused code
+package groupanagrams
+
+/*
+	時間：3分
+	変数名をリファクタした。
+
+	質問：
+		- resという変数名をどう思いますか？
+		- mという変数名をどう思いますか？（mapのmです）
+		- wordsという変数名をどう思いますか？
+*/
+
+func groupAnagrams_step3(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+	for _, word := range strs {
+		var freq [26]int
+		for _, r := range word {
+			freq[r-'a']++
+		}
+		m[freq] = append(m[freq], word)
+	}
+
+	res := make([][]string, len(m))
+	i := 0
+	for _, words := range m {
+		res[i] = words
+		i++
+	}
+	return res
+}
