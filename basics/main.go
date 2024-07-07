704fc18814097525637b2c63d175e120fb1c3f31
rihib <rihib@rihib.dev>
2024-07-07 16:10:26 +0900

    refactor basics/main.go

--- basics/main.go
+++ basics/main.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
 
 	"github.com/rihib/leetcode/basics/sort"
 )
 
 func main() {
-	nums := []int{4, 3, 2, 1}
-	sorted := sort.Insertion(nums)
-	print(sorted)
+	testcases := [][]int{
+		{},
+		{1},
+		{1, 1},
+		{3, 2, 1},
+		{3, 2, 2, 1},
+		{4, 3, 2, 1},
+		{4, 4, 3, 3, 2, 2, 1, 1},
+	}
+
+	runSort(sort.Insertion, testcases)
+	runSort(sort.Selection, testcases)
+}
+
+func runSort(f func([]int) []int, testcases [][]int) {
+	fmt.Println(getFunctionName(f))
+	for _, nums := range testcases {
+		sorted := f(nums)
+		print(sorted)
+	}
+	fmt.Print("\n")
+}
+
+func getFunctionName(f interface{}) string {
+	full := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	n := strings.TrimPrefix(full, "github.com/rihib/leetcode/basics/sort.")
+	return n
 }
 
 func print(nums []int) {
+	if len(nums) == 0 {
+		fmt.Print("none")
+	}
 	for _, n := range nums {
 		fmt.Printf("%d ", n)
 	}
