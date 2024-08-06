e3d897175e124eb1b0c893a1871bf746b5ca00f1
rihib <rihib@rihib.dev>
2024-08-06 15:25:39 +0900

    refactor Add Two Numbers

--- go/add_two_numbers.go
+++ go/add_two_numbers.go
@@ -15,8 +15,8 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			sum += l2.Val
 			l2 = l2.Next
 		}
-		carry = sum / 10
 		curr.Next = &ListNode{Val: sum % 10}
+		carry = sum / 10
 		curr = curr.Next
 	}
 	return dummy.Next
