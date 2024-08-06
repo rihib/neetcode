b3bc7f665d03532ecec2e36bea8e37ee0095eb5d
rihib <rihib@rihib.dev>
2024-08-06 14:40:20 +0900

    refactor Add Two Numbers

--- go/add_two_numbers.go
+++ go/add_two_numbers.go
@@ -6,29 +6,20 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummy.Next = new(ListNode)
 	dummy.Next.Val = 0
 	curr := dummy
-	for l1 != nil && l2 != nil {
+	for l1 != nil || l2 != nil {
 		curr = curr.Next
-		sum := l1.Val + l2.Val + curr.Val
+		sum := curr.Val
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
 		curr.Val = sum % 10
 		curr.Next = new(ListNode)
 		curr.Next.Val = sum / 10
-		l1, l2 = l1.Next, l2.Next
-	}
-	for l1 != nil {
-		curr = curr.Next
-		sum := l1.Val + curr.Val
-		curr.Val = sum % 10
-		curr.Next = new(ListNode)
-		curr.Next.Val = sum / 10
-		l1 = l1.Next
-	}
-	for l2 != nil {
-		curr = curr.Next
-		sum := l2.Val + curr.Val
-		curr.Val = sum % 10
-		curr.Next = new(ListNode)
-		curr.Next.Val = sum / 10
-		l2 = l2.Next
 	}
 	if curr.Next.Val == 0 {
 		curr.Next = nil
