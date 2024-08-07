e651d20d2794ccf8eaad0f64144ee568184a37fc
rihib <rihib@rihib.dev>
2024-08-07 13:06:17 +0900

    refactor Zigzag Conversion

--- go/zigzag_conversion.go
+++ go/zigzag_conversion.go
@@ -1,24 +1,26 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
+import "strings"
+
 func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
 
-	rows := make([][]rune, numRows)
+	rows := make([]strings.Builder, numRows)
 	cycleLen := 2*numRows - 2
 	for i, r := range s {
 		rowNum := i % cycleLen
 		if rowNum >= numRows {
 			rowNum = cycleLen - rowNum
 		}
-		rows[rowNum] = append(rows[rowNum], r)
+		rows[rowNum].WriteRune(r)
 	}
 
-	var oneline []rune
+	var oneline strings.Builder
 	for _, row := range rows {
-		oneline = append(oneline, row...)
+		oneline.WriteString(row.String())
 	}
-	return string(oneline)
+	return oneline.String()
 }
