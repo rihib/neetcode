27d422c1e8e684091717b68a7ef6cca1d7b1735a
rihib <rihib@rihib.dev>
2024-08-08 22:40:35 +0900

    refactor Generate Parentheses

--- go/generate_parentheses.go
+++ go/generate_parentheses.go
@@ -4,23 +4,23 @@ package main
 func generateParenthesis(n int) []string {
 	var parentheses []string
 	var stack []byte
-	var backtrack func(int, int)
-	backtrack = func(open int, closed int) {
+	var generate func(int, int)
+	generate = func(open int, closed int) {
 		if open == n && closed == n {
 			parentheses = append(parentheses, string(stack))
 			return
 		}
 		if open < n {
 			stack = append(stack, '(')
-			backtrack(open+1, closed)
+			generate(open+1, closed)
 			stack = stack[:len(stack)-1]
 		}
 		if open > closed {
 			stack = append(stack, ')')
-			backtrack(open, closed+1)
+			generate(open, closed+1)
 			stack = stack[:len(stack)-1]
 		}
 	}
-	backtrack(0, 0)
+	generate(0, 0)
 	return parentheses
 }
