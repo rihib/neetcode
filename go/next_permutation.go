1364d91b96070a3fcabf9788ce18d76141048024
rihib <rihib@rihib.dev>
2024-08-09 00:59:17 +0900

    refactor Next Permutation

--- go/next_permutation.go
+++ go/next_permutation.go
@@ -6,15 +6,13 @@ import "sort"
 func nextPermutation(nums []int) {
 	for i := len(nums) - 1; i > 0; i-- {
 		if nums[i-1] < nums[i] {
-			minN := nums[i]
-			idx := i
+			candidate, candidateIndex := nums[i], i
 			for j := i; j < len(nums); j++ {
-				if nums[j] < minN && nums[i-1] < nums[j] {
-					idx = j
-					minN = nums[j]
+				if nums[j] < candidate && nums[i-1] < nums[j] {
+					candidate, candidateIndex = nums[j], j
 				}
 			}
-			nums[i-1], nums[idx] = nums[idx], nums[i-1]
+			nums[i-1], nums[candidateIndex] = nums[candidateIndex], nums[i-1]
 			sort.Slice(nums[i:], func(a, b int) bool {
 				return nums[i+a] < nums[i+b]
 			})
