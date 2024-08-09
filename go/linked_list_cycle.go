119af5e281dfeddff54d569e218e969df324b5d3
rihib <rihib@rihib.dev>
2024-08-09 16:04:15 +0900

    add comment

--- go/linked_list_cycle.go
+++ go/linked_list_cycle.go
@@ -1,6 +1,7 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
+// `https://github.com/ryo-devz/LeetCode/pull/1#discussion_r1710718113`に書かれているとおり、印をつける方法はデメリットが大きい
 func hasCycle(head *ListNode) bool {
 	fast, slow := head, head
 	for fast != nil && fast.Next != nil {
