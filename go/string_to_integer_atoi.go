add3803b948a64a6de19de78b6c134cbcf577373
rihib <rihib@rihib.dev>
2024-08-09 15:44:36 +0900

    fix  String to Integer (atoi)

--- go/string_to_integer_atoi.go
+++ go/string_to_integer_atoi.go
@@ -24,14 +24,14 @@ func myAtoi(s string) int {
 
 	num := 0
 	for i < len(s) && '0' <= s[i] && s[i] <= '9' {
-		next := int(s[i] - '0')
-		if sign != -1 && num > (intMax-next)/10 {
+		digit := int(s[i] - '0')
+		if sign == 1 && (num > intMax/10 || num == intMax/10 && digit >= intMax%10) {
 			return intMax
 		}
-		if sign == -1 && -num < (intMin+next)/10 {
+		if sign == -1 && (-num < intMin/10 || -num == intMin/10 && -digit <= intMin%10) {
 			return intMin
 		}
-		num = num*10 + next
+		num = num*10 + digit
 		i++
 	}
 	return sign * num
