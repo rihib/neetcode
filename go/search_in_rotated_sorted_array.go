9667dcc58b24ff0ef80aadcb7a75e2bdae2bd689
rihib <rihib@rihib.dev>
2024-08-09 18:17:25 +0900

    Search in Rotated Sorted Array

---
+++ go/search_in_rotated_sorted_array.go
@@ -0,0 +1,26 @@
+//lint:file-ignore U1000 Ignore all unused code
+package main
+
+func searchInRotatedSortedArray(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if target == nums[mid] {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
