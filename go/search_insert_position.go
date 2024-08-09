c5f0404f086b84c11c706818a91150fcf58f97a0
rihib <rihib@rihib.dev>
2024-08-09 22:29:33 +0900

    refactor Search Insert Position

--- go/search_insert_position.go
+++ go/search_insert_position.go
@@ -4,7 +4,7 @@ package main
 func searchInsert(nums []int, target int) int {
 	left, right := 0, len(nums)
 	for left < right {
-		mid := (left + right) / 2
+		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
 		}
