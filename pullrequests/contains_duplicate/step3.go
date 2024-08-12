c7da8fc7687ab7cc86cf8b016ab62a698bfb2ec5
rihib <rihib@rihib.dev>
2024-08-12 16:44:08 +0900

    add step3 for Contains Duplicate

---
+++ pullrequests/contains_duplicate/step3.go
@@ -0,0 +1,13 @@
+//lint:file-ignore U1000 Ignore all unused code
+package containsduplicate
+
+func containsDuplicate(nums []int) bool {
+	seen := make(map[int]struct{})
+	for _, n := range nums {
+		if _, ok := seen[n]; ok {
+			return true
+		}
+		seen[n] = struct{}{}
+	}
+	return false
+}
