82d9c897e4669c7216e726983b8ee756b80af160
rihib <rihib@rihib.dev>
2024-08-12 21:13:00 +0900

    refactor Valid Anagram

--- go/valid_anagram.go
+++ go/valid_anagram.go
@@ -2,21 +2,18 @@
 package main
 
 func isAnagram(s string, t string) bool {
-	var frequency [26]int
+	var frequencyS, frequencyT [26]int
 	for _, r := range s {
-		frequency[r-'a']++
+		frequencyS[r-'a']++
 	}
 	for _, r := range t {
-		frequency[r-'a']--
+		frequencyT[r-'a']++
 	}
-	for _, n := range frequency {
-		if n != 0 {
-			return false
-		}
-	}
-	return true
+	return frequencyS == frequencyT
 }
 
+// ちゃんとUnicodeに対応させるなら結合文字などを考慮する必要がある
+// https://github.com/rihib/leetcode/pull/5#discussion_r1706198268
 func isAnagram_unicode(s string, t string) bool {
 	frequency := make(map[rune]int)
 	for _, r := range s {
