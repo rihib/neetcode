562b955dad6e25d7c80fe28cad042ccc94e6f213
rihib <rihib@rihib.dev>
2024-08-12 21:56:16 +0900

    add step4 for Zigzag Conversion

---
+++ pullrequests/zigzag_conversion/step4.go
@@ -0,0 +1,47 @@
+//lint:file-ignore U1000 Ignore all unused code
+package zigzagconversion
+
+import "strings"
+
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+
+	rows := make([]strings.Builder, numRows)
+	cycleLength := 2*numRows - 2
+	for i, r := range s {
+		rowIndex := i % cycleLength
+		if rowIndex >= numRows {
+			rowIndex = cycleLength - rowIndex
+		}
+		rows[rowIndex].WriteRune(r)
+	}
+
+	var oneline strings.Builder
+	for _, row := range rows {
+		oneline.WriteString(row.String())
+	}
+	return oneline.String()
+}
+
+func convert_anothersolution(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	var oneline strings.Builder
+	cycleLength := 2*numRows - 2
+	for rowIndex := 0; rowIndex < numRows; rowIndex++ {
+		for i := rowIndex; i < len(s); i += cycleLength {
+			oneline.WriteByte(s[i])
+			if rowIndex == 0 || rowIndex == numRows-1 {
+				continue
+			}
+			diagIndex := i + cycleLength - rowIndex*2
+			if diagIndex < len(s) {
+				oneline.WriteByte(s[diagIndex])
+			}
+		}
+	}
+	return oneline.String()
+}
