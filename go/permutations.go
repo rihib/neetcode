666c78e8f4b54be0fa025ba91c6f0a144d2446a8
rihib <rihib@rihib.dev>
2024-08-13 14:05:31 +0900

    Permutations

---
+++ go/permutations.go
@@ -0,0 +1,26 @@
+//lint:file-ignore U1000 Ignore all unused code
+package main
+
+func permute(nums []int) [][]int {
+	var permutations [][]int
+	var stack []int
+	var generate func(map[int]struct{})
+	generate = func(inUse map[int]struct{}) {
+		if len(stack) == len(nums) {
+			permutations = append(permutations, append([]int{}, stack...))
+			return
+		}
+		for _, n := range nums {
+			if _, ok := inUse[n]; !ok {
+				stack = append(stack, n)
+				inUse[n] = struct{}{}
+				generate(inUse)
+				stack = stack[:len(stack)-1]
+				delete(inUse, n)
+			}
+		}
+	}
+	inUse := make(map[int]struct{})
+	generate(inUse)
+	return permutations
+}
