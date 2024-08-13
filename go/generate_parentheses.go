b4ccfd7dc544beb66da828b24f59a7535b0dce44
rihib <rihib@rihib.dev>
2024-08-13 16:48:31 +0900

    add another solution for Generate Parentheses

--- go/generate_parentheses.go
+++ go/generate_parentheses.go
@@ -1,7 +1,36 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func generateParenthesis(n int) []string {
+func generateParenthesis_iterative(n int) []string {
+	var parentheses []string
+	type state struct {
+		parenthesis []byte
+		open        int
+		closed      int
+	}
+	stack := []state{{[]byte{}, 0, 0}}
+	for len(stack) > 0 {
+		current := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if current.open == n && current.closed == n {
+			parentheses = append(parentheses, string(current.parenthesis))
+			continue
+		}
+		if current.open < n {
+			newParenthesis := append([]byte{}, current.parenthesis...)
+			newParenthesis = append(newParenthesis, '(')
+			stack = append(stack, state{newParenthesis, current.open + 1, current.closed})
+		}
+		if current.open > current.closed {
+			newParenthesis := append([]byte{}, current.parenthesis...)
+			newParenthesis = append(newParenthesis, ')')
+			stack = append(stack, state{newParenthesis, current.open, current.closed + 1})
+		}
+	}
+	return parentheses
+}
+
+func generateParenthesis_recursive(n int) []string {
 	var parentheses []string
 	parenthesis := make([]byte, 0, n*2)
 	var generate func(int, int)
