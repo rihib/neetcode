43fc2ff707a0e44912b877b9920577d11edf9038
rihib <rihib@rihib.dev>
2024-08-15 23:41:59 +0900

    refactor Is Subsequence

--- go/is_subsequence.go
+++ go/is_subsequence.go
@@ -1,19 +1,15 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func isSubsequence(s string, t string) bool {
+func isSubsequence2(s string, t string) bool {
 	current := 0
-	for i := 0; i < len(s); i++ {
-		for {
-			if current >= len(t) {
-				return false
-			}
-			if s[i] == t[current] {
-				current++
-				break
-			}
+	for i := 0; i < len(t); i++ {
+		if s[current] == t[i] {
 			current++
 		}
+		if current == len(s) {
+			return true
+		}
 	}
-	return true
+	return false
 }
