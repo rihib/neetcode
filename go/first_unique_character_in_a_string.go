c7559284a5a2d2875ee740ec6826daaf1cfe8f66
rihib <rihib@rihib.dev>
2024-08-15 14:14:00 +0900

    add another solution for First Unique Character in a String

--- go/first_unique_character_in_a_string.go
+++ go/first_unique_character_in_a_string.go
@@ -13,3 +13,16 @@ func firstUniqChar(s string) int {
 	}
 	return -1
 }
+
+func firstUniqChar2(s string) int {
+	var frequency [26]int
+	for _, r := range s {
+		frequency[r-'a']++
+	}
+	for i, r := range s {
+		if frequency[r-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}
