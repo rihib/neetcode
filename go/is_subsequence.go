2642db49db05064637040876ac069b774896fe52
rihib <rihib@rihib.dev>
2024-08-15 23:50:05 +0900

    refactor Is Subsequence

--- go/is_subsequence.go
+++ go/is_subsequence.go
@@ -2,17 +2,12 @@
 package main
 
 func isSubsequence(s string, t string) bool {
-	if len(s) == 0 {
-		return true
-	}
-	current := 0
-	for i := 0; i < len(t); i++ {
-		if s[current] == t[i] {
-			current++
+	i, j := 0, 0
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
 		}
-		if current == len(s) {
-			return true
-		}
+		j++
 	}
-	return false
+	return i == len(s)
 }
