a35d3ce17bdf3662a07c8b787bafbb5be2d9e477
rihib <rihib@rihib.dev>
2024-08-17 22:08:20 +0900

    add another solution for Product of Array Except Self

--- go/product_of_array_except_self.go
+++ go/product_of_array_except_self.go
@@ -18,3 +18,31 @@ func productExceptSelf(nums []int) []int {
 
 	return res
 }
+
+func productExceptSelf2(nums []int) []int {
+	allProduct, zeroCount := 0, 0
+	for _, n := range nums {
+		if n == 0 {
+			zeroCount++
+			continue
+		}
+		if allProduct == 0 {
+			allProduct = n
+		} else {
+			allProduct *= n
+		}
+	}
+	products := make([]int, len(nums))
+	for i, _ := range products {
+		if zeroCount > 0 {
+			if nums[i] == 0 && zeroCount == 1 {
+				products[i] = allProduct
+			} else {
+				products[i] = 0
+			}
+		} else {
+			products[i] = allProduct / nums[i]
+		}
+	}
+	return products
+}
