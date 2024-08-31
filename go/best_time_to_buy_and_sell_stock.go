63a52df3a28c89093a3b0e1390754f445d5ab2f2
rihib <rihib@rihib.dev>
2024-08-31 19:25:11 +0900

    refactor Best Time to Buy and Sell Stock

--- go/best_time_to_buy_and_sell_stock.go
+++ go/best_time_to_buy_and_sell_stock.go
@@ -2,10 +2,10 @@
 package main
 
 func maxProfit(prices []int) int {
-	lowestPrice, highestProfit := prices[0], 0
-	for _, n := range prices {
-		lowestPrice = min(lowestPrice, n)
-		highestProfit = max(highestProfit, n-lowestPrice)
+	minPrice, maxProfit := prices[0], 0
+	for _, price := range prices {
+		minPrice = min(minPrice, price)
+		maxProfit = max(maxProfit, price-minPrice)
 	}
-	return highestProfit
+	return maxProfit
 }
