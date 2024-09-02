f4d1e4b74fda5630cf43c939d7fd09ca71a99333
rihib <rihib@rihib.dev>
2024-09-02 22:23:38 +0900

    refactor Binary Search

--- go/binary_search.go
+++ go/binary_search.go
@@ -1,21 +1,34 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func search(nums []int, target int) int {
-	l, r := 0, len(nums)-1
-	for l <= r {
-		mid := l + (r-l)/2
-
-		if target == nums[mid] {
+func binarySearchHalfClosed(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
 			return mid
 		}
-
 		if target < nums[mid] {
-			r = mid - 1
+			right = mid
 		} else {
-			l = mid + 1
+			left = mid + 1
 		}
 	}
+	return -1
+}
 
+func binarySearchClosed(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if target < nums[mid] {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
 	return -1
 }
