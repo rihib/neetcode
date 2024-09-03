ef711b73487f5f07a4710a6053fc0a5193181e2e
rihib <rihib@rihib.dev>
2024-09-03 13:01:52 +0900

    add another solution of Lowest Common Ancestor of a Binary Search Tree

--- go/lowest_common_ancester_of_a_binary_search_tree.go
+++ go/lowest_common_ancester_of_a_binary_search_tree.go
@@ -1,14 +1,28 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+func lowestCommonAncestorIterative(root, p, q *TreeNode) *TreeNode {
+	node := root
+	for node != nil {
+		if p.Val <= node.Val && node.Val <= q.Val || q.Val <= node.Val && node.Val <= p.Val {
+			return node
+		}
+		if p.Val < node.Val && q.Val < node.Val {
+			node = node.Left
+		}
+		if node.Val < p.Val && node.Val < q.Val {
+			node = node.Right
+		}
+	}
+	return nil
+}
+
+func lowestCommonAncestorRecursive(root, p, q *TreeNode) *TreeNode {
 	if p.Val < root.Val && q.Val < root.Val {
-		return lowestCommonAncestor(root.Left, p, q)
+		return lowestCommonAncestorRecursive(root.Left, p, q)
 	}
-
 	if root.Val < p.Val && root.Val < q.Val {
-		return lowestCommonAncestor(root.Right, p, q)
+		return lowestCommonAncestorRecursive(root.Right, p, q)
 	}
-
 	return root
 }
