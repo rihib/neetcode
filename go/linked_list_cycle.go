f3c33b46632a3b8440b499f4d4bedfc6e5abcf3b
rihib <rihib@rihib.dev>
2024-09-06 10:09:48 +0900

    refactor Linked List Cycle

--- go/linked_list_cycle.go
+++ go/linked_list_cycle.go
@@ -1,17 +1,6 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func hasCycleFloyd(head *ListNode) bool {
-	slow, fast := head, head
-	for fast != nil && fast.Next != nil {
-		slow, fast = slow.Next, fast.Next.Next
-		if slow == fast {
-			return true
-		}
-	}
-	return false
-}
-
 func hasCycleMap(head *ListNode) bool {
 	seen := make(map[*ListNode]struct{})
 	node := head
@@ -21,6 +10,17 @@ func hasCycleMap(head *ListNode) bool {
 		}
 		seen[node] = struct{}{}
 		node = node.Next
+	}
+	return false
+}
+
+func hasCycleFloyd(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true
+		}
 	}
 	return false
 }
