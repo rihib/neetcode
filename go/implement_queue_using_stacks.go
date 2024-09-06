683df75796d096ffd41b74d725771cde02e5f12f
rihib <rihib@rihib.dev>
2024-09-06 15:05:06 +0900

    refactor Implement Queue using Stacks

--- go/implement_queue_using_stacks.go
+++ go/implement_queue_using_stacks.go
@@ -1,35 +1,36 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
+// `implement_queue_using_stacks2.go`に実際に書く場合のコードを記載
 type MyQueue struct {
-	InputStack  []int
-	OutputStack []int
+	pushStack []int
+	popStack  []int
 }
 
 func Constructor() MyQueue {
 	return MyQueue{}
 }
 
-func (q *MyQueue) Push(x int) {
-	q.InputStack = append(q.InputStack, x)
+func (q *MyQueue) Push(n int) {
+	q.pushStack = append(q.pushStack, n)
 }
 
 func (q *MyQueue) Pop() int {
-	x := q.Peek()
-	q.OutputStack = q.OutputStack[:len(q.OutputStack)-1]
-	return x
+	n := q.Peek()
+	q.popStack = q.popStack[:len(q.popStack)-1]
+	return n
 }
 
 func (q *MyQueue) Peek() int {
-	if len(q.OutputStack) == 0 {
-		for len(q.InputStack) > 0 {
-			q.OutputStack = append(q.OutputStack, q.InputStack[len(q.InputStack)-1])
-			q.InputStack = q.InputStack[:len(q.InputStack)-1]
+	if len(q.popStack) == 0 {
+		for len(q.pushStack) > 0 {
+			q.popStack = append(q.popStack, q.pushStack[len(q.pushStack)-1])
+			q.pushStack = q.pushStack[:len(q.pushStack)-1]
 		}
 	}
-	return q.OutputStack[len(q.OutputStack)-1]
+	return q.popStack[len(q.popStack)-1]
 }
 
 func (q *MyQueue) Empty() bool {
-	return len(q.InputStack) == 0 && len(q.OutputStack) == 0
+	return len(q.pushStack) == 0 && len(q.popStack) == 0
 }
