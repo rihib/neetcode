15a283cdfe9ee75746ad9aaae149a46688616f55
rihib <rihib@rihib.dev>
2024-09-06 09:13:26 +0900

    refactor Linked List Cycle

--- go/linked_list_cycle.go
+++ go/linked_list_cycle.go
@@ -3,10 +3,10 @@ package main
 
 // `https://github.com/ryo-devz/LeetCode/pull/1#discussion_r1710718113`に書かれているとおり、印をつける方法はデメリットが大きい
 func hasCycle(head *ListNode) bool {
-	fast, slow := head, head
+	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
-		fast, slow = fast.Next.Next, slow.Next
-		if fast == slow {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
 			return true
 		}
 	}
