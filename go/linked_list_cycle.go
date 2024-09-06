0b7820af5fb4fcd09f9ce9b47938b2204bd5155a
rihib <rihib@rihib.dev>
2024-09-06 09:32:16 +0900

    add another solution of Linked List Cycle

--- go/linked_list_cycle.go
+++ go/linked_list_cycle.go
@@ -2,13 +2,26 @@
 package main
 
 // `https://github.com/ryo-devz/LeetCode/pull/1#discussion_r1710718113`に書かれているとおり、印をつける方法はデメリットが大きい
-func hasCycle(head *ListNode) bool {
+func hasCycleFloyd(head *ListNode) bool {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow, fast = slow.Next, fast.Next.Next
 		if slow == fast {
 			return true
 		}
+	}
+	return false
+}
+
+func hasCycleMap(head *ListNode) bool {
+	seen := make(map[*ListNode]struct{})
+	node := head
+	for node != nil {
+		if _, ok := seen[node]; ok {
+			return true
+		}
+		seen[node] = struct{}{}
+		node = node.Next
 	}
 	return false
 }
