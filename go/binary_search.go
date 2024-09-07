a869367db7c301234117b22d7e0e6f73204f5bd5
rihib <rihib@rihib.dev>
2024-09-07 20:06:49 +0900

    add another solution of Binary Search

--- go/binary_search.go
+++ go/binary_search.go
@@ -19,7 +19,58 @@ func binarySearchHalfClosed(nums []int, target int) int {
 	return -1
 }
 
-func binarySearchClosed(nums []int, target int) int {
+func binarySearchHalfClosed2(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for {
+		if left == right {
+			break
+		}
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if target < nums[mid] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func binarySearchHalfClosed3(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
+}
+
+func binarySearchHalfClosed4(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	if right >= len(nums) || nums[right] != target {
+		return -1
+	}
+	return right
+}
+
+func binarySearchClosed1(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := left + (right-left)/2
@@ -33,6 +84,22 @@ func binarySearchClosed(nums []int, target int) int {
 		}
 	}
 	return -1
+}
+
+func binarySearchClosed2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
 }
 
 func search(nums []int, target int) int {
