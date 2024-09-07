bd49e64c6fcd148185b0713fafc83f3d42f490c2
rihib <rihib@rihib.dev>
2024-09-07 16:38:17 +0900

    add another solution of First Bad Version

--- go/first_bad_version.go
+++ go/first_bad_version.go
@@ -7,12 +7,25 @@ func isBadVersion(version int) bool {
 	return version == BadVersion
 }
 
-func firstBadVersion(n int) int {
+func firstBadVersionHalfClosed(n int) int {
 	left, right := 1, n+1
 	for left < right {
 		mid := left + (right-left)/2
 		if isBadVersion(mid) {
 			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func firstBadVersionClosed(n int) int {
+	left, right := 1, n
+	for left <= right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid - 1
 		} else {
 			left = mid + 1
 		}
