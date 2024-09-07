6df2a36439a8321efa232d0038523891b65c4d43
rihib <rihib@rihib.dev>
2024-09-07 19:36:34 +0900

    pullrequests/first_bad_version

--- pullrequests/first_bad_version/step1.go
+++ pullrequests/first_bad_version/step1.go
@@ -17,13 +17,13 @@ func isBadVersion(version int) bool {
 
 単なる二分探索だが、少し混乱してしまったため、時間がかかってしまった。二分探索については理解しているつもりだが、考えずにできるレベルにはまだなれてないので少し問題文が変更されていると頭で考える必要が出てきてしまう。
 
-半閉区画の場合、rightは常にtrueの範囲に収まり続け、leftはfalseとtrueの境界にあるとき、mid+1によりtrueに移動するため、right == leftになった瞬間はleftはFirst bad versionであることがわかる。
+半閉区画の場合、rightは常にtrueの範囲に収まり続け、leftはfalseとtrueの境界にあるとき、mid+1によりtrueに移動するため、left == rightになった瞬間はleftはFirst bad versionであることがわかる。
 leftがmid+1でtrueに移動したときにrightがleftよりも大きい位置（left < right）にいたとしても、探索は続くが、leftもrightもtrueであればmidも必ずtrueになるため、leftは動かず、rightだけが徐々にleftに近づいていき、最終的にはleftと合流する。
-right = midと更新される以上、rightがleftを飛び越えることはなくtrueの範囲にとどまり続け、left == rightになるまで探索は終了しないので、半閉区画の場合はleftの代わりにrightを返しても問題ない。
+right = midと更新される以上、rightがleftを飛び越えることはなくtrueの範囲にとどまり続け、left == rightになるまで探索は終了しないので、firstBadVersionHalfClosed2のようにleftの代わりにrightを返しても問題ない。
 rightがleftを飛び越えることはないのでfirstBadVersionHalfClosed3のように書くこともできる。
 
-閉区画の場合、falseとtrueの境界にあるとき、rightはmid-1によりfalseの方に移動するのに対し、leftはtrueの方に移動するため、right < leftになった瞬間にleftはFirst bad versionであることがわかる。right <= leftとしてしまうと、trueの範囲内でleft == rightになる場合は問題ないが、falseの範囲内でleft == rightとなる場合は探索結果になってしまう。
-right < leftになった瞬間に探索が終了するわけだが、この場合、leftはtrue、rightはfalseになっているため、閉区画の場合は必ずleftを返す必要がある。
+閉区画の場合、falseとtrueの境界にあるとき、rightはmid-1によりfalseの方に移動するのに対し、leftはtrueの方に移動するため、right < leftになった瞬間にleftはFirst bad versionであることがわかる。left == rightの場合、trueの範囲内でleft == rightになる場合は問題ないが、falseの範囲内でleft == rightとなる場合は探索結果になってしまうため、right < leftになるまで探索を続ける必要がある。
+right < leftになった瞬間に探索が終了するわけだが、このとき、leftはtrue、rightはfalseになっているため、閉区画の場合は必ずleftを返す必要がある。
 */
 func firstBadVersionHalfClosed1(n int) int {
 	left, right := 1, n+1
