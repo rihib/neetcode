91648694f379bad523c08af38ce4293c606214d1
rihib <rihib@rihib.dev>
2024-09-22 01:53:18 +0900

    add another solution for Majority Element

--- go/majority_element.go
+++ go/majority_element.go
@@ -2,6 +2,17 @@
 package main
 
 func majorityElement(nums []int) int {
+	frequencies := make(map[int]int, len(nums))
+	for _, n := range nums {
+		frequencies[n]++
+		if frequencies[n] > len(nums)/2 {
+			return n
+		}
+	}
+	return -1
+}
+
+func majorityElementBoyerMooreMajorityVote(nums []int) int {
 	candidate, count := nums[0], 0
 	for _, n := range nums {
 		if n == candidate {
