066ae12a2b107939dd4762d99e0ddbdcee7bad03
rihib <rihib@rihib.dev>
2024-09-26 22:49:46 +0900

    refactor Middle of the Linked List

--- go/middle_of_the_linked_list.go
+++ go/middle_of_the_linked_list.go
@@ -2,15 +2,12 @@
 package main
 
 func middleNode(head *ListNode) *ListNode {
-	if head == nil || head.Next == nil {
-		return head
+	if head == nil {
+		return nil
 	}
-
 	slow, fast := head, head
-
 	for fast != nil && fast.Next != nil {
 		slow, fast = slow.Next, fast.Next.Next
 	}
-
 	return slow
 }
