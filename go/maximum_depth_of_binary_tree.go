f9dcfc81bfbf994926e08908c95b724912c164a2
rihib <rihib@rihib.dev>
2024-09-26 23:31:20 +0900

    refactor Maximum Depth of Binary Tree

--- go/maximum_depth_of_binary_tree.go
+++ go/maximum_depth_of_binary_tree.go
@@ -2,11 +2,10 @@
 package main
 
 func maxDepth(root *TreeNode) int {
-	// TestCase: [], [0], [0, 1], [0, 1, 2], [0, 1, null, 2]
 	if root == nil {
 		return 0
 	}
-
-	maxdepth := max(maxDepth(root.Left), maxDepth(root.Right)) + 1
-	return maxdepth
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	return max(leftDepth, rightDepth) + 1
 }
