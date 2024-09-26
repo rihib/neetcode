f11269b659109f4d24131fa75648ec38cca0d562
rihib <rihib@rihib.dev>
2024-09-26 23:43:10 +0900

    add another solution of Middle of the Linked List

--- go/middle_of_the_linked_list.go
+++ go/middle_of_the_linked_list.go
@@ -2,6 +2,20 @@
 package main
 
 func middleNode(head *ListNode) *ListNode {
+	count := 0
+	node := head
+	for node != nil {
+		node = node.Next
+		count++
+	}
+	middle := head
+	for i := 0; i < count/2; i++ {
+		middle = middle.Next
+	}
+	return middle
+}
+
+func middleNode2(head *ListNode) *ListNode {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow, fast = slow.Next, fast.Next.Next
