fd827c0a1dea242eeb5b457a338e508a010cdf13
rihib <rihib@rihib.dev>
2024-10-15 10:33:34 +0900

    refactor Roman to Integer

--- go/roman_to_integer.go
+++ go/roman_to_integer.go
@@ -2,26 +2,54 @@
 package main
 
 func romanToInt(s string) int {
-	symbols := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
+	symbolToInt := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
+	total := 0
+	runeS := []rune(s)
+	currIndex, nextIndex := 0, 1
+	for currIndex < len(runeS) && nextIndex < len(runeS) {
+		curr, next := runeS[currIndex], runeS[nextIndex]
+		if (curr == 'I' && (next == 'V' || next == 'X')) ||
+			(curr == 'X' && (next == 'L' || next == 'C')) ||
+			(curr == 'C' && (next == 'D' || next == 'M')) {
+			total += symbolToInt[next] - symbolToInt[curr]
+			currIndex, nextIndex = currIndex+2, nextIndex+2
+			continue
+		}
+		total += symbolToInt[curr]
+		currIndex, nextIndex = currIndex+1, nextIndex+1
+	}
+	if currIndex < len(runeS) {
+		total += symbolToInt[runeS[currIndex]]
+	}
+	return total
+}
 
-	res := 0
-	runes := []rune(s)
-	for i := 0; i < len(runes); i++ {
-		if i+1 < len(runes) &&
-			symbols[string(runes[i])] < symbols[string(runes[i+1])] {
-			res -= symbols[string(runes[i])]
+func romanToInt2(s string) int {
+	symbolToInt := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	total := 0
+	runeS := []rune(s)
+	for i, r := range runeS {
+		if i+1 < len(runeS) && symbolToInt[r] < symbolToInt[runeS[i+1]] {
+			total -= symbolToInt[r]
 		} else {
-			res += symbols[string(runes[i])]
+			total += symbolToInt[r]
 		}
 	}
-
-	return res
+	return total
 }
