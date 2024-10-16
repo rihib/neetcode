d66cfc56fcee65213d7145451b9112476d4a92bd
rihib <rihib@rihib.dev>
2024-10-16 21:13:30 +0900

    refactor Counting Bits

--- go/counting_bits.go
+++ go/counting_bits.go
@@ -1,14 +1,32 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
+// 2の冪乗の時はMSBのみが1になる
+// それ以外の場合はMSB以外の部分はすでに前に計算していてメモ化されているのでそれを使えばいい
 func countBits(n int) []int {
-	res := make([]int, n+1)
-	powerOf2 := 1
+	bitCounts := make([]int, n+1)
+	powerOfTwo := 1
 	for i := 1; i <= n; i++ {
-		if powerOf2*2 == i {
-			powerOf2 = i
+		if i == powerOfTwo*2 {
+			powerOfTwo = i
 		}
-		res[i] = 1 + res[i-powerOf2]
+		bitCounts[i] = 1 + bitCounts[i-powerOfTwo]
 	}
-	return res
+	return bitCounts
+}
+
+// 内側のループはnを2で割り続けるのでlog n回
+// 外側のループはn回なので、全体でO(n log n)になる
+func countBits2(n int) []int {
+	bitCounts := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		num := i
+		count := 0
+		for num > 0 {
+			count += num % 2
+			num /= 2
+		}
+		bitCounts[i] = count
+	}
+	return bitCounts
 }
