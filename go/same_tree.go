d3e529a81d94705d049c92bad07377720a1b1bd2
rihib <rihib@rihib.dev>
2024-10-16 22:18:31 +0900

    refactor Same Tree

--- go/same_tree.go
+++ go/same_tree.go
@@ -2,14 +2,11 @@
 package main
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	// TestCase: [[], []], [[1], []], [[1], [1]], [[1, 2, 3], [1, 2]]
 	if p == nil && q == nil {
 		return true
 	}
-
 	if p == nil || q == nil || p.Val != q.Val {
 		return false
 	}
-
 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
