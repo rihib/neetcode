9a5dc12203467ea668d776309c1604c0eeced966
rihib <rihib@rihib.dev>
2024-10-19 17:16:50 +0900

    refactor Number of 1 bits

--- go/number_of_1bits.go
+++ go/number_of_1bits.go
@@ -1,11 +1,38 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func hammingWeight(n int) int {
+func hammingWeightNaive1(n int) int {
 	count := 0
 	for n > 0 {
 		count += n % 2
 		n /= 2
+	}
+	return count
+}
+
+func hammingWeightNaive2(n int) int {
+	count := 0
+	for n != 0 {
+		count += n & 1
+		n >>= 1
+	}
+	return count
+}
+
+func hammingWeightBitmanipulation(n int) int {
+	count := 0
+	for n > 0 {
+		n &= n - 1
+		count += 1
+	}
+	return count
+}
+
+func hammingWeight2Complement(n int) int {
+	count := 0
+	for n > 0 {
+		n -= (n & -n)
+		count += 1
 	}
 	return count
 }
