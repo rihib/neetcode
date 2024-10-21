15f0c1c1940a5d259099b13ed5adbc469e6e73c4
rihib <rihib@rihib.dev>
2024-10-21 23:48:05 +0900

    refactor Longest Common Prefix

--- go/longest_common_prefix.go
+++ go/longest_common_prefix.go
@@ -1,13 +1,36 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-import "sort"
+import (
+	"sort"
+)
 
-func longestCommonPrefix(strs []string) string {
-	if len(strs) == 1 {
-		return strs[0]
+func longestCommonPrefix1(strs []string) string {
+	if len(strs) == 0 {
+		return ""
 	}
+	var prefix []rune
+	for i := 0; ; i++ {
+		if i >= len(strs[0]) {
+			return string(prefix)
+		}
+		curr := strs[0][i]
+		for _, word := range strs {
+			if i >= len(word) {
+				return string(prefix)
+			}
+			if word[i] != curr {
+				return string(prefix)
+			}
+		}
+		prefix = append(prefix, rune(curr))
+	}
+}
 
+func longestCommonPrefix2(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
 	sort.Strings(strs)
 	for i := range strs[0] {
 		if strs[0][i] != strs[len(strs)-1][i] {
