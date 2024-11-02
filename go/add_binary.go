2f0fc67c91d86214b99dd4e3d6ce01171617b3b4
rihib <rihib@rihib.dev>
2024-11-02 23:08:05 +0900

    refactor Add Binary

--- go/add_binary.go
+++ go/add_binary.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -12,14 +13,14 @@ func addBinary(a string, b string) string {
 	for i := 1; i <= len(a) || i <= len(b); i++ {
 		bitA, bitB := 0, 0
 		if i <= len(a) {
-			bitA = int(a[len(a)-i] - '0') // a[len(a)-i]がbyte型なので'0'もbyte型と解釈さ
+			bitA, _ = strconv.Atoi(string(a[len(a)-i]))
 		}
 		if i <= len(b) {
-			bitB = int(b[len(b)-i] - '0')
+			bitB, _ = strconv.Atoi(string(b[len(b)-i]))
 		}
 		sum := bitA + bitB + carry
 		carry = sum / 2
-		reversed.WriteByte(byte(sum%2 + '0')) // sum%2がint型なので'0'もint型として扱われる
+		reversed.WriteString(strconv.Itoa(sum % 2))
 	}
 	if carry == 1 {
 		reversed.WriteByte('1')
