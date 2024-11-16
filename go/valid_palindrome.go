3c095384d618537b902c80396f6c52e2b1f8bf14
rihib <rihib@rihib.dev>
2024-11-16 18:12:22 +0900

    refactor Valid Palindrome

--- go/valid_palindrome.go
+++ go/valid_palindrome.go
@@ -5,21 +5,21 @@ import "unicode"
 
 func isPalindrome(s string) bool {
 	runeS := []rune(s)
-	left, right := 0, len(s)-1
-	for left < right {
-		if !(unicode.IsDigit(runeS[left]) || unicode.IsLetter(runeS[left])) {
-			left++
+	i, j := 0, len(s)-1
+	for i < j {
+		if !unicode.IsDigit(runeS[i]) && !unicode.IsLetter(runeS[i]) {
+			i++
 			continue
 		}
-		if !(unicode.IsDigit(runeS[right]) || unicode.IsLetter(runeS[right])) {
-			right--
+		if !unicode.IsDigit(runeS[j]) && !unicode.IsLetter(runeS[j]) {
+			j--
 			continue
 		}
-		if unicode.ToLower(runeS[left]) != unicode.ToLower(runeS[right]) {
+		if unicode.ToLower(runeS[i]) != unicode.ToLower(runeS[j]) {
 			return false
 		}
-		left++
-		right--
+		i++
+		j--
 	}
 	return true
 }
