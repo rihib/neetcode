0983bff8be31b6d4fe9d99a21d1f1a5658a3e253
rihib <rihib@rihib.dev>
2024-11-17 13:27:40 +0900

    Climbing Stairs

--- go/climbing_stairs.go
+++ go/climbing_stairs.go
@@ -2,8 +2,8 @@
 package main
 
 func climbStairsDP(n int) int {
-	prev, curr := 0, 1
-	for i := 1; i <= n; i++ {
+	prev, curr := 1, 1
+	for i := 1; i < n; i++ {
 		prev, curr = curr, prev+curr
 	}
 	return curr
@@ -15,8 +15,8 @@ func climbStairsmemorizedRecursion(n int) int {
 }
 
 func climbStairsHelper(n int, m map[int]int) int {
-	if n == 1 || n == 2 {
-		return n
+	if n == 0 || n == 1 {
+		return 1
 	}
 	if ways, ok := m[n]; ok {
 		return ways
