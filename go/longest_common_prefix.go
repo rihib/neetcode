65625f34a9c71a2f360cf693e7fabf10e918ba95
rihib <rihib@rihib.dev>
2024-11-18 00:18:16 +0900

    add another solution for Longest Common Prefix

--- go/longest_common_prefix.go
+++ go/longest_common_prefix.go
@@ -36,3 +36,53 @@ func longestCommonPrefix2(strs []string) string {
 	}
 	return strs[0]
 }
+
+/*
+Trie
+*/
+type Trie struct {
+	root *Node
+}
+
+type Node struct {
+	children  map[rune]*Node
+	isWordEnd bool
+}
+
+func NewTrie() *Trie {
+	return &Trie{root: &Node{children: make(map[rune]*Node)}}
+}
+
+func (t *Trie) Insert(word string) {
+	node := t.root
+	for _, r := range word {
+		if _, ok := node.children[r]; !ok {
+			node.children[r] = &Node{children: make(map[rune]*Node)}
+		}
+		node = node.children[r]
+	}
+	node.isWordEnd = true
+}
+
+func (t *Trie) Prefix() string {
+	node := t.root
+	var prefix []rune
+	for len(node.children) == 1 && !node.isWordEnd {
+		for r, child := range node.children {
+			prefix = append(prefix, r)
+			node = child // ここでnodeをchildで更新してもrangeは評価済みなのでループが続くことはない
+		}
+	}
+	return string(prefix)
+}
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	trie := NewTrie()
+	for _, word := range strs {
+		trie.Insert(word)
+	}
+	return trie.Prefix()
+}
