737705fec4d2ad79e44ffe637f04ba21c22a39c9
rihib <rihib@rihib.dev>
2024-12-21 14:30:35 +0900

    refactor Palindrome Number

--- go/palindrome_number.go
+++ go/palindrome_number.go
@@ -1,15 +1,30 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func isPalindromeNumber(x int) bool {
-	if x < 0 {
-		return false
-	}
+import "strconv"
 
-	original, reversed := x, 0
-	for x > 0 {
-		reversed = reversed*10 + x%10
-		x /= 10
+func isPalindromeNumbver1(x int) bool {
+	var reversed int
+	tmp := x
+	for tmp > 0 {
+		n := tmp % 10
+		reversed = reversed*10 + n
+		tmp /= 10
 	}
-	return original == reversed
+	return x == reversed
+}
+
+// Itoaは数字を文字列に変換するのでエラーが起きることはないため、返り値としてerrorは返らない。
+// Atoiは文字列を数字に変換するので変換エラーが起きる可能性があり、返り値としてerrorも返る。
+func isPalindromeNumber2(x int) bool {
+	strX := strconv.Itoa(x)
+	i, j := 0, len(strX)-1
+	for i < j {
+		if strX[i] != strX[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
