8a1167abd3d58707cbd0945e7412ec7c679f524d
rihib <rihib@rihib.dev>
2025-02-24 15:40:39 +0900

    refactor move zeroes

--- go/move_zeroes.go
+++ go/move_zeroes.go
@@ -1,11 +1,6 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-// 左側に非ゼロのエリア、右側にゼロのエリアを作れば良い。
-// イメージとしてはzeroIndexはゼロのエリアのはじまり（境界）のインデックスを持ち、
-// for文で探索していって非ゼロの要素を見つけたらzeroIndexの要素とスワップして、
-// zeroIndexをインクリメントすることで、zeroIndexの左側に非ゼロの要素を集め、
-// 右側にゼロを集めるという感じ
 func moveZeroes(nums []int) {
 	zeroIndex := 0
 	for i, n := range nums {
