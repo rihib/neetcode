cc8c9309fcd64c2faf9eefa68a60772045ae5f44
za-bannai <r.bannai@zebra-ai.net>
2025-03-05 14:03:09 +0900

    add another solution of add_two_numbers

--- go/add_two_numbers.go
+++ go/add_two_numbers.go
@@ -1,7 +1,9 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+// dummyを使うことで最後に値0を持つノードを作らずに済む
+// 単純にl1とl2の数を求めて足し算する方法だと、64個以上のノード数を持つリスト同士の足し算ができない
+func addTwoNumbersIteratice(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummy := new(ListNode)
 	node := dummy
 	carry := 0
@@ -20,4 +22,26 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		node = node.Next
 	}
 	return dummy.Next
+}
+
+func addTwoNumbersRecursive(l1 *ListNode, l2 *ListNode) *ListNode {
+	return addTwoNumbersHelper(l1, l2, 0)
+}
+
+func addTwoNumbersHelper(l1 *ListNode, l2 *ListNode, carry int) *ListNode {
+	if l1 == nil && l2 == nil && carry == 0 {
+		return nil
+	}
+	l1Val, l2Val := 0, 0
+	if l1 != nil {
+		l1Val = l1.Val
+		l1 = l1.Next
+	}
+	if l2 != nil {
+		l2Val = l2.Val
+		l2 = l2.Next
+	}
+	sum := l1Val + l2Val + carry
+	head := &ListNode{Val: sum % 10, Next: addTwoNumbersHelper(l1, l2, sum/10)}
+	return head
 }
